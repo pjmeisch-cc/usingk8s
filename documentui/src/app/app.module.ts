e86a0b770da5c31152572873fafdf53862b459ad
P.J. Meisch <peter-josef.meisch@codecentric.de>
2017-11-03 15:25:46 +0100

    added navigation and sub-components

--- documentui/src/app/app.module.ts
+++ documentui/src/app/app.module.ts
@@ -1,16 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {SearchComponent} from './search.component';
+import {DetailComponent} from './detail.component';
+
+const appRoutes: Routes = [
+  { path: 'search', component: SearchComponent },
+  { path: 'detail', component: DetailComponent},
+  { path: '', redirectTo: '/search', pathMatch: 'full'},
+  { path: '**', redirectTo: '/search', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchComponent,
+    DetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
